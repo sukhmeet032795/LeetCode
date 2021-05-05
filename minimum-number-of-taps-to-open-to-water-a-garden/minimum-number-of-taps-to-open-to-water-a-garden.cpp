b555b4acfc7044dd2df679677cb298d696e8133e
Sukhmeet Singh <sukhmeet032795@gmail.com>
2021-05-06 00:25:29 +0530

    Time: 8 ms (97.29%), Space: 13.9 MB (100.00%) - LeetHub

--- minimum-number-of-taps-to-open-to-water-a-garden/minimum-number-of-taps-to-open-to-water-a-garden.cpp
+++ minimum-number-of-taps-to-open-to-water-a-garden/minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -1,21 +1,41 @@
 class Solution {
 public:
+    
+    int jump(vector<int>& nums) {
+        
+        int len = nums.size();
+        
+        if(len == 1)
+            return 0;
+        
+        int end = 0, i = 0, maxEnd = 0, steps = 0;
+        
+        for( ; i <= (len - 1); )
+        {
+            steps++;
+            for( ; i <= end; i++)
+            {
+                int tmp = nums[i];
+                maxEnd = max(maxEnd, tmp);
+                
+                if(maxEnd >= (len - 1))
+                    return steps;
+            }
+            
+            if(end == maxEnd)
+                return -1;
+            end = maxEnd;
+        };
+        
+        return -1;
+    }
+    
     int minTaps(int n, vector<int>& a) {
         
-        for(int i=0;i<a.size();i++){
+        for(int i=0;i< a.size();i++){
             a[max(0,i-a[i])] = max(a[max(0,i-a[i])], i+a[i]);
         }
         
-        int maxdis=0, jmps=0, cur=0;
-        for(int i=0;i<a.size()-1;i++){
-            cur=max(cur, a[i]);
-            if(i==maxdis){
-                if(cur == maxdis) return -1;
-                jmps++;
-                maxdis=cur;
-            }
-        }
-        
-        return jmps;
+        return jump(a);
     }
 };
