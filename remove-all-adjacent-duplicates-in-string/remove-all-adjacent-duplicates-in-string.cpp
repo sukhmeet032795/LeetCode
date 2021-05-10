626eafceaf09b1d587392ac54472c1ba157be55c
Sukhmeet Singh <sukhmeet032795@gmail.com>
2021-05-10 21:16:18 +0530

    Time: 424 ms (5.76%), Space: 779.7 MB (5.30%) - LeetHub

--- remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
+++ remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
@@ -1,3 +1,71 @@
+// class Solution {
+// public:
+    
+//     struct node{
+//         char _char;
+//         int _count;
+//         node(char ch, int count) : _char(ch), _count(count) {};
+//     };
+    
+//     string removeDuplicates(string S) {
+      
+//         int len = S.length();
+//         if(len == 0)
+//             return "";
+        
+//         stack<node> s;
+//         for(int i = 0; i < len; )
+//         {
+//             if(s.empty())
+//             {
+//                 s.push(node(S[i], 1));
+//                 i++;
+//             }
+//             else
+//             {
+//                 if(!s.empty() && S[i] == s.top()._char)
+//                 {
+//                     s.top()._count += 1;
+//                     i++;
+//                     continue;
+//                 }
+//                 else if(!s.empty() && S[i] != s.top()._char)
+//                 {
+//                     if(s.top()._count > 1)
+//                     {
+//                         if(s.top()._count % 2 == 0)
+//                             s.pop();
+//                         else
+//                             s.top()._count = 1;
+//                         continue;
+//                     }
+//                 }
+//                 s.push(node(S[i], 1));
+//                 i++;
+//             }
+//         };
+        
+//         while(!s.empty() && s.top()._count > 1)
+//         {
+//             if(s.top()._count % 2 == 0)
+//                 s.pop();
+//             else{
+//                 s.top()._count = 1;
+//                 break;
+//             }
+//         }
+        
+//         string out = "";
+//         while(!s.empty())
+//         {
+//             out = s.top()._char + out;
+//             s.pop();
+//         }
+        
+//         return out;
+//     }
+// };
+
 class Solution {
 public:
     
