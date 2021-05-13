511a3b2fbab22666e270a8edb663bf1a4ad5a1ce
Sukhmeet Singh <sukhmeet032795@gmail.com>
2021-05-13 22:21:32 +0530

    Time: 8 ms (22.41%), Space: 9.9 MB (13.70%) - LeetHub

--- verify-preorder-serialization-of-a-binary-tree/verify-preorder-serialization-of-a-binary-tree.cpp
+++ verify-preorder-serialization-of-a-binary-tree/verify-preorder-serialization-of-a-binary-tree.cpp
@@ -1,44 +1,44 @@
 class Solution {
 public:
     
-    struct treeNode{
-        int data;
-        treeNode* left;
-        treeNode* right;
-        treeNode(int d) : data(d), left(NULL), right(NULL) {};
-    };
+//     struct treeNode{
+//         int data;
+//         treeNode* left;
+//         treeNode* right;
+//         treeNode(int d) : data(d), left(NULL), right(NULL) {};
+//     };
     
-    treeNode* generateTree(int &currInd, int &length, vector<string> &data, bool &check)
-    {
-        if(currInd >= length)
-            return NULL;
+//     treeNode* generateTree(int &currInd, int &length, vector<string> &data, bool &check)
+//     {
+//         if(currInd >= length)
+//             return NULL;
         
-        if(data[currInd] == "#"){
-            currInd += 1;
-            return NULL;
-        };
+//         if(data[currInd] == "#"){
+//             currInd += 1;
+//             return NULL;
+//         };
         
-        treeNode* val = new treeNode(stoi(data[currInd]));
-        currInd += 1;
+//         treeNode* val = new treeNode(stoi(data[currInd]));
+//         currInd += 1;
         
-        if(currInd >= length)
-        {
-            check = false;
-            return val;
-        }
+//         if(currInd >= length)
+//         {
+//             check = false;
+//             return val;
+//         }
         
-        val->left = generateTree(currInd, length, data, check);
+//         val->left = generateTree(currInd, length, data, check);
         
-        if(currInd >= length)
-        {
-            check = false;
-            return val;
-        }
+//         if(currInd >= length)
+//         {
+//             check = false;
+//             return val;
+//         }
         
-        val->right = generateTree(currInd, length, data, check);
+//         val->right = generateTree(currInd, length, data, check);
         
-        return val;
-    };
+//         return val;
+//     };
     
     vector<string> getData(string preorder)
     {
@@ -56,15 +56,32 @@ public:
     
     bool isValidSerialization(string preorder) {
         
-        bool check = true;
-        vector<string> data = getData(preorder);
-        int currInd = 0, length = data.size();
-        
-        generateTree(currInd, length, data, check);
-        
-        if(currInd < length)
-            return false;
-        
-        return check;
+        vector<string> strs = getData(preorder);
+        int cnt = 0;
+        for (int i = 0; i < strs.size(); ++i) {
+            if (strs[i] == "#") {
+                if (cnt == 0) {
+                    return i == (strs.size() - 1);
+                }
+                --cnt;
+            } else {
+                ++cnt;
+            }
+        }
+        return false;
     }
+    
+//     bool isValidSerialization(string preorder) {
+        
+//         bool check = true;
+//         vector<string> data = getData(preorder);
+//         int currInd = 0, length = data.size();
+        
+//         generateTree(currInd, length, data, check);
+        
+//         if(currInd < length)
+//             return false;
+        
+//         return check;
+//     }
 };
