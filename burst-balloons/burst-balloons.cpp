db816cff337674263ca50e50bbe130b3a15f3ff6
Sukhmeet Singh <sukhmeet032795@gmail.com>
2022-01-02 20:45:08 +0530

    Time: 604 ms (57.31%), Space: 10.4 MB (9.38%) - LeetHub

--- burst-balloons/burst-balloons.cpp
+++ burst-balloons/burst-balloons.cpp
@@ -6,21 +6,17 @@ public:
         nums.insert(nums.end(), 1);
         
         int len = nums.size();
-        vector<vector<int>> dp(len+1, vector<int>(len+1, 0));
+        vector< vector<int> > dp(len+1, vector<int>(len+1, 0));
         
-        for(int l = 2; l <= len; l++)
-        {
-            for(int i = 1; i <= (len - l); i++)
-            {
+        for (int l = 2; l <= nums.size(); l++) {
+            for (int i = 1; i <= (len - l); i++) {
                 int j = i + l - 1;
                 dp[i][j] = INT_MIN;
-                for(int k = i; k < j && j < len; k++)
-                {
-                    dp[i][j] = max(dp[i][j], dp[i][k] + dp[k+1][j] + nums[i-1]*nums[k]*nums[j]);
-                };
-            };
-        };
-        
+                for (int k = i; k < j && j < len; k++) {
+                    dp[i][j] = max(dp[i][j], dp[i][k] + dp[k+1][j] + nums[i-1]*nums[j]*nums[k]);
+                }
+            }
+        }
         return dp[1][len-1];
     }
 };
