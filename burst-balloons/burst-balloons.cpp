8239969576570fcfe82aec3d07318735ead0200f
Sukhmeet Singh <sukhmeet032795@gmail.com>
2022-01-02 15:18:03 +0530

    Time: 1268 ms (5.09%), Space: 10.3 MB (26.84%) - LeetHub

---
+++ burst-balloons/burst-balloons.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxCoins(vector<int>& nums) {
+        
+        nums.insert(nums.begin(), 1);
+        nums.insert(nums.end(), 1);
+        
+        int len = nums.size();
+        vector<vector<int>> dp(len+1, vector<int>(len+1, 0));
+        
+        for(int l = 2; l <= len; l++)
+        {
+            for(int i = 1; i <= (len - l); i++)
+            {
+                int j = i + l - 1;
+                dp[i][j] = INT_MIN;
+                for(int k = i; k < j && j < len; k++)
+                {
+                    dp[i][j] = max(dp[i][j], dp[i][k] + dp[k+1][j] + nums[i-1]*nums[k]*nums[j]);
+                };
+            };
+        };
+        
+        return dp[1][len-1];
+    }
+};
