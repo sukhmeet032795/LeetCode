23268d01cf91475e446365781bb65fd8cfabc85d
Sukhmeet Singh <sukhmeet032795@gmail.com>
2022-02-02 22:11:45 +0530

    Time: 58 ms (19.47%), Space: 14.2 MB (9.71%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
@@ -9,22 +9,32 @@ public:
         map<char, int> m1, m2;
         for(int i = 0; i < p.length(); i++) {
             m1[p[i]]++;
-            m2[s[i]]++;
         }
         
-        if(m1 == m2)
-            out.push_back(0);
-        
-        int start = 0, end = p.length();
+        int start = 0, end = 0, count = 0;
         while(end < s.length()) {
-            m2[s[start]]--;
-            if(m2[s[start]] == 0)
-                m2.erase(s[start]);
-            m2[s[end]]++;
             
-            if(m1 == m2)
+            if(m1.find(s[end]) == m1.end()) {
+                end++;
+                start = end;
+                count = 0;
+                m2.clear();
+                continue;
+            }
+            
+            m2[s[end]]++;
+            count++;
+            
+            if(count == p.length() && m1 == m2)
                 out.push_back(end - p.length() + 1);
-            start++;
+            
+            while(count >= p.length()) {
+                m2[s[start]]--;
+                if(m2[s[start]] == 0)
+                    m2.erase(s[start]);
+                start++;
+                count--;
+            }
             end++;
         }
         
