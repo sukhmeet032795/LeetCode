6279974f29275c0c2349ff93b5c219484634419d
Sukhmeet Singh <sukhmeet032795@gmail.com>
2022-06-23 00:28:40 +0530

    Time: 33 ms (34.22%), Space: 19.7 MB (13.27%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -3,12 +3,13 @@ public:
     int majorityElement(vector<int>& nums) {
         
         unordered_map<int, int> m;
+        int majority = ceil((float)nums.size()/2);
         for(auto num : nums) {
             m[num]++;
         };
         
         for(auto ele : m) {
-            if (ele.second >= ceil((float)nums.size()/2))
+            if (ele.second >= majority)
                 return ele.first;
         }
         
