cd939c54502157a76fae8592ccbec272d755d6e6
Sukhmeet Singh <sukhmeet032795@gmail.com>
2022-07-09 14:24:34 +0530

    Time: 6 ms (58.53%), Space: 7.2 MB (90.78%) - LeetHub

--- 541-reverse-string-ii/541-reverse-string-ii.cpp
+++ 541-reverse-string-ii/541-reverse-string-ii.cpp
@@ -5,11 +5,6 @@ public:
         if (k == 1) return s;
         
         int len = s.length();
-        if (k >= len) {
-            reverse(s.begin(), s.end());
-            return s;
-        };
-        
         for(int i = 0; i < len; i += (k*2)) {
             if ((i + k) >= len) {
                 reverse(s.begin() + i, s.end());
