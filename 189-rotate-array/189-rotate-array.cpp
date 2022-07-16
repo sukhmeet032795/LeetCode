f0f2dde78aa122dc090b779939248361791c22b9
Sukhmeet Singh <sukhmeet032795@gmail.com>
2022-07-16 11:15:07 +0530

    Time: 48 ms (46.45%), Space: 24.9 MB (81.45%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -1,16 +1,7 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        
-        vector<int> arr = nums;
-        int n = arr.size();
-        
-        if(n == 0 || n == 1)
-            return;
-        
-        for(int i = 0; i < n; i++)
-        {
-            nums[(i+k)%n] = arr[i];
-        }
+        k = k % nums.size();
+        ::rotate(nums.begin(), nums.begin()+(nums.size() - k), nums.end());
     }
 };
