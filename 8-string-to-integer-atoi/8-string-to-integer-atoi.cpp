d19e99ecf01c91691d83e87dae9cfc7cbc202e7e
Sukhmeet Singh <sukhmeet032795@gmail.com>
2022-07-23 15:01:37 +0530

    Time: 3 ms (82.67%), Space: 7.1 MB (52.96%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
@@ -1,81 +1,45 @@
 class Solution {
 public:
-    int myAtoi(string str) {
+    int myAtoi(string s) {
         
-        long int ans = 0;
-        int number = 0, sign = 0;
-        bool neg = false, numSet = false;
+        long int num = 0;
+        bool neg = false, space = false, isNumSet = false, pos = false;
         
-        for(int i = 0; i < str.length(); i++)
-        {
-            if(str[i] == ' ')
-            {
-                if(numSet){
-                    if(neg == false)
-                        return static_cast<int>(ans);
-                    else
-                    {
-                        ans = 0 - ans;
-                        return static_cast<int>(ans);
-                    }
-                }
-                
-                if(sign > 0)
-                    return 0;
-                
+        for(int i = 0; i < s.length(); i++) {
+            
+            if (s[i] == ' ') {  
+                if (neg || pos)
+                    break;
+                if (!isNumSet)
+                    continue;
+                break;
+            } else if (s[i] == '-') {
+                if (neg || pos || isNumSet)
+                    break;
+                neg = true;
                 continue;
-            };
-            
-            if(str[i] == '+' || str[i] == '-')
-            {
-                if(numSet){
-                    if(neg == false)
-                        return static_cast<int>(ans);
-                    else
-                    {
-                        ans = 0 - ans;
-                        return static_cast<int>(ans);
-                    }
-                }
-                
-                if(sign >= 1)
-                    return 0;
-                sign++;
-                neg = (str[i] == '+' ? false : true);
+            } else if (s[i] == '+') {
+                if (neg || pos || isNumSet)
+                    break;
+                pos = true;
                 continue;
-            };
-            
-            if(str[i] < '0' || str[i] > '9')
-            {
-                if(numSet){
+            } else if (!isdigit(s[i])) {
+                break;
+            } else {
+                int tmp = s[i] - '0';
+                num = num*10 + tmp;
+                isNumSet = true;
+                
+                if((num) > (INT_MAX))
+                {
                     if(neg == false)
-                        return static_cast<int>(ans);
+                        return INT_MAX;
                     else
-                    {
-                        ans = 0 - ans;
-                        return static_cast<int>(ans);
-                    }
-                }
-                return 0;
-            };
-            
-            int num = str[i] - '0';
-            ans = ans*10 + num;
-            numSet = true;
-            
-            if((ans) > (INT_MAX))
-            {
-                if(neg == false)
-                    return INT_MAX;
-                else
-                    return INT_MIN;
-            };
+                        return INT_MIN;
+                };
+            }
         };
-        
-        if(neg)
-        {
-            ans = 0 - ans;
-        };
-        return static_cast<int>(ans);;
+        num = ((neg) ? (-num) : (num));
+        return static_cast<int>(num);
     }
 };
