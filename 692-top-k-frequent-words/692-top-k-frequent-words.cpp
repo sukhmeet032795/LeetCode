efb7a1834934c46bc6e2b78ff91410edf57ecf73
Sukhmeet Singh <sukhmeet032795@gmail.com>
2022-08-05 22:37:07 +0530

    Time: 10 ms (95.26%), Space: 12.7 MB (34.13%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.cpp
+++ 692-top-k-frequent-words/692-top-k-frequent-words.cpp
@@ -2,36 +2,32 @@ class Solution {
 public:
     
     struct customSort{
-        bool operator()(const pair<int, string> &p1, const pair<int, string> &p2) {
-            if (p1.first == p2.first) {
-                return p1.second > p2.second;
+        bool operator()(const pair<string, int>& p1, const pair<string, int>& p2) {
+            if (p1.second == p2.second) {
+                return p1.first > p2.first;
             };
-            return p1.first < p2.first;
+            return p1.second < p2.second;
         };
     };
     
     vector<string> topKFrequent(vector<string>& words, int k) {
         
-        priority_queue<pair<int, string>, vector<pair<int, string>>, customSort> p;
         unordered_map<string, int> um;
-        
         for(auto word : words) {
             um[word]++;
         };
         
-        for(auto itr : um) {
-            p.push(make_pair(itr.second, itr.first));
+        priority_queue<pair<string, int>, vector<pair<string, int>>, customSort> p;
+        for(auto word: um) {
+            p.push(make_pair(word.first, word.second));
         };
-        
-        um.clear();
         
         vector<string> out;
-        int ind = 0;
-        while(ind++ < k) {
-            out.push_back(p.top().second);
+        while(k--) {
+            auto tmp = p.top();
             p.pop();
+            out.emplace_back(tmp.first);
         };
-        
         return out;
     }
 };
