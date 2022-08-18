ab63113f884b80d9ac0b84365c7c9404aa22066c
Sukhmeet Singh <sukhmeet032795@gmail.com>
2022-08-18 09:29:44 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (63.84%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
@@ -13,27 +13,50 @@ class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
         
-        if (!root) return {};
+//         if (!root) return {};
         
-        stack<TreeNode*> s;
-        TreeNode* curr = root;
+//         stack<TreeNode*> s;
+//         TreeNode* curr = root;
         
-        vector<int> out;
+//         vector<int> out;
         
-        while(!s.empty() || curr!= NULL) {
-            if (curr) {
-                out.push_back(curr->val);
-                s.push(curr);
-                curr = curr->right;
-            }
-            else {
-                curr = s.top();
-                s.pop();
-                curr = curr->left;
-            }
-        }
+//         while(!s.empty() || curr!= NULL) {
+//             if (curr) {
+//                 out.push_back(curr->val);
+//                 s.push(curr);
+//                 curr = curr->right;
+//             }
+//             else {
+//                 curr = s.top();
+//                 s.pop();
+//                 curr = curr->left;
+//             }
+//         }
         
-        reverse(out.begin(), out.end());
-        return out;
+//         reverse(out.begin(), out.end());
+//         return out;
+        
+        vector<int> output;
+        if (!root) return output;
+        
+        stack<TreeNode*> ss;
+        ss.push(root);
+        
+        while(!ss.empty()) {
+            
+            auto top = ss.top();
+            ss.pop();
+            
+            if (top->left)
+                ss.push(top->left);
+            
+            if (top->right)
+                ss.push(top->right);
+            
+            output.emplace_back(top->val);
+        };
+        
+        reverse(output.begin(), output.end());
+        return output;
     }
 };
