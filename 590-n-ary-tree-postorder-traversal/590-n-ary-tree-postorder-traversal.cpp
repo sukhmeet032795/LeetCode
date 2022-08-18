85191a2df2149d8075a6f4061629432f19032591
Sukhmeet Singh <sukhmeet032795@gmail.com>
2022-08-18 09:28:14 +0530

    Time: 25 ms (84.52%), Space: 11.1 MB (80.21%) - LeetHub

--- 590-n-ary-tree-postorder-traversal/590-n-ary-tree-postorder-traversal.cpp
+++ 590-n-ary-tree-postorder-traversal/590-n-ary-tree-postorder-traversal.cpp
@@ -20,21 +20,41 @@ public:
 
 class Solution {
 public:
-    void postOrder(Node* root, vector<int>& output) {
+//     void postOrder(Node* root, vector<int>& output) {
         
-        if (!root)
-            return;
+//         if (!root)
+//             return;
         
-        for(int i = 0; i < root->children.size(); i++) {
-              postOrder(root->children[i], output);  
-        };
-        output.emplace_back(root->val);
-    };
+//         for(int i = 0; i < root->children.size(); i++) {
+//               postOrder(root->children[i], output);  
+//         };
+//         output.emplace_back(root->val);
+//     };
+    
+//     vector<int> postorder(Node* root) {
+        
+//         vector<int> output;
+//         postOrder(root, output);
+//         return output;
+//     }
     
     vector<int> postorder(Node* root) {
         
         vector<int> output;
-        postOrder(root, output);
+        if (!root) return output;
+        
+        stack<Node*> ss;
+        ss.push(root);
+        
+        while(!ss.empty()) {
+            
+            auto top = ss.top();
+            ss.pop();
+            for(auto itr : top->children) { ss.push(itr); }
+            output.emplace_back(top->val);
+        };
+        
+        reverse(output.begin(), output.end());
         return output;
     }
 };
