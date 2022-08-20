865c9144fc1f7cb92ec3fb43288cf7040c54589f
Sukhmeet Singh <sukhmeet032795@gmail.com>
2022-08-20 15:59:27 +0530

    Time: 23 ms (27.10%), Space: 21.4 MB (39.51%) - LeetHub

--- 112-path-sum/112-path-sum.cpp
+++ 112-path-sum/112-path-sum.cpp
@@ -12,36 +12,11 @@
 class Solution {
 public:
     
-    void traverseTree(TreeNode* root, bool &ans, int &targetSum, int sum) {
-        
-        if(root == NULL) {
-            if(sum == targetSum)
-                ans = true;
-            return;
-        }
-        
-        sum += root->val;
-        if(root->left == NULL && root->right == NULL)
-        {
-            if(sum == targetSum)
-                ans = true;
-            return;
-        }
-        
-        if(root->left)
-            traverseTree(root->left, ans, targetSum, sum);
-        
-        if(root->right)
-            traverseTree(root->right, ans, targetSum, sum);
-    }
-    
     bool hasPathSum(TreeNode* root, int targetSum) {
         
-        if(root == NULL)
-            return false;
-        
-        bool check = false;
-        traverseTree(root, check, targetSum, 0);
-        return check;
+        if(!root)return false;                                         
+        targetSum=targetSum-root->val;                                             
+        if(targetSum==0&&!root->left&&!root->right)return true;              
+        return hasPathSum(root->left,targetSum)||hasPathSum(root->right,targetSum);
     }
 };
