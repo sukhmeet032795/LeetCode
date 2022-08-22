b582fbf1653c2dfa419a3b659303389e3ea2b297
Sukhmeet Singh <sukhmeet032795@gmail.com>
2022-08-22 09:45:56 +0530

    Time: 24 ms (85.68%), Space: 14 MB (53.95%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -4,35 +4,31 @@ public:
         
         vector<vector<int>> graph(numCourses);
         vector<int> inDegree(numCourses, 0);
-        
+
         for(auto itr : prerequisites) {
-            graph[itr[0]].emplace_back(itr[1]);
-            inDegree[itr[1]]++;
+            graph[itr[1]].emplace_back(itr[0]);
+            inDegree[itr[0]]++;
         };
         
-        vector<int> ans;
-        queue<int> nodes;
+        queue<int> q;
         for(int itr = 0; itr < inDegree.size(); itr++) {
-            if (inDegree[itr] == 0) { 
-                nodes.push(itr);
+            if (inDegree[itr] == 0) {
+                q.push(itr);
             };
         };
         
-        if (nodes.size() == 0) { return false; }
-        
-        while(!nodes.empty()) {
-            
-            int curr = nodes.front();
-            nodes.pop();
-            ans.emplace_back(curr);
-            for(int i = 0; i < graph[curr].size(); i++) {
-                inDegree[graph[curr][i]]--;
-                if (inDegree[graph[curr][i]] == 0) {
-                    nodes.push(graph[curr][i]);
-                };
+        vector<int> output;
+        while(!q.empty()) {
+            int curr = q.front();
+            q.pop();
+            output.emplace_back(curr);
+            for(auto itr1 : graph[curr]) {
+                inDegree[itr1]--;
+                if (inDegree[itr1] == 0)
+                    q.push(itr1);
             };
         };
         
-        return (ans.size() == numCourses);
+        return (output.size() == numCourses);
     }
 };
