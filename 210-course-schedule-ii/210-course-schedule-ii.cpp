610413fd17005b4fd6c79226252523a8bcd12e64
Sukhmeet Singh <sukhmeet032795@gmail.com>
2022-08-22 09:47:06 +0530

    Time: 35 ms (51.25%), Space: 14.1 MB (46.73%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        
+        vector<vector<int>> graph(numCourses);
+        vector<int> inDegree(numCourses, 0);
+
+        for(auto itr : prerequisites) {
+            graph[itr[1]].emplace_back(itr[0]);
+            inDegree[itr[0]]++;
+        };
+        
+        queue<int> q;
+        for(int itr = 0; itr < inDegree.size(); itr++) {
+            if (inDegree[itr] == 0) {
+                q.push(itr);
+            };
+        };
+        
+        vector<int> output;
+        while(!q.empty()) {
+            int curr = q.front();
+            q.pop();
+            output.emplace_back(curr);
+            for(auto itr1 : graph[curr]) {
+                inDegree[itr1]--;
+                if (inDegree[itr1] == 0)
+                    q.push(itr1);
+            };
+        };
+        
+        if (output.size() == numCourses)
+            return output;
+        
+        return {};
+    }
+};
